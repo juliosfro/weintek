7323e8ee5abc34e4d93f4934f0ef3ba5cbe9c8d1
Julio de Oliveira Neto <juliodeoliveiraneto@Julios-MacBook-Pro.local>
2025-03-22 14:21:20 -0300

    Exemplo de uso da lib converte ASCII para UTF-8

---
+++ TextView/exemplo-de-uso-convert-ascii-utf-8.js
@@ -0,0 +1,13 @@
+const convert = require('/convert-ascii-utf8.js');
+
+// Enderecos das tags de entrada (vindas do CLP) a serem convertidas para utf-8
+const inputTagAddresses = [
+    this.config.TagName_M_9_36,
+];
+
+// Enderecos das tags de saida (enviadas para a IHM) a serem gravadas na tag local da IHM
+const outputTagAddresses = [
+    this.config.StrOutput,  
+];
+
+convert.processMultipleInputs(inputTagAddresses, outputTagAddresses);
